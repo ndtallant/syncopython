6abc3fc041e4e44997ab761740d5783ef9de37f5
Nick Tallant <ndtallant@uchicago.edu>
2018-02-08 02:06:56 -0600

    Created a drummer

--- __init__.py
+++ __init__.py
@@ -25,3 +25,36 @@ class RhythmString(object):
 
     def __repr__(self):
         return self.rhythm
+
+class Drummer(object):
+    '''
+    This object is a collection of 4 RhythmStrings.
+    Lots of defaults to come!
+    '''
+
+    def __init__(self, RH="eighths", 
+                       LH="2 4", 
+                       RF="1 3", 
+                       LF="quarters",
+                       tempo=88):
+
+        self.RH = RhythmString(RH,tempo)
+        self.LH = RhythmString(LH,tempo)
+        self.RF = RhythmString(RF,tempo)
+        self.LF = RhythmString(LF,tempo)
+        self.tempo = tempo
+
+    @property
+    def tempo(self):
+        return self._tempo
+    
+    @tempo.setter
+    def tempo(self, tempo):
+        if not isinstance(tempo, int) or tempo < 0:
+            raise ValueError("Great drummers use a positive integer \
+                    for tempo")
+        self._tempo = tempo
+        self.RH.tempo = tempo
+        self.LH.tempo = tempo
+        self.RF.tempo = tempo
+        self.LF.tempo = tempo
