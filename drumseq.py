05b2a76bf22637a22cecabbc1c41c1df77dcc479
Nick Tallant <ndtallant@uchicago.edu>
2018-03-02 22:55:14 -0600

    Saving original drumseq file and creating a new one to develop/understand backend

--- drumseq.py
+++ drumseq.py
@@ -2,6 +2,8 @@
 This should be a (possibly doctored)
 version of rtmidi/examples/drumseq/drumseq.py
 '''
+# This file currently uses a lot of code from a
+# MIDI Drum sequencer prototype, by Michiel Overtoom, motoom@xs4all.nl
 
 class Sequencer():
     pass
