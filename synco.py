fe69a4a44ad69a4232a42ebcbbf6c8dd575c2da7
Ross Heflin <heflin.rosst@gmail.com>
2018-03-16 18:13:19 -0500

    plays pattern when you pass -i '64 | 1,2,3,4 | Bassdrum' to ./synco.py
    - can use -i pattern or --input=pattern to specify your pattern
    - output.play() gets called properly in the context manager
      and doesn't terminate early
    - output.stop() should get called when cntrl+c gets pressed

--- synco.py
+++ synco.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-from transcription import Transcription, RhythmString 
+
+# stdlib
+import threading
+import time
+
+# 3rd party
 import docopt
-import drumseq_orig
-import contextlib
+import rtmidi
+from rtmidi.midiutil import open_midioutput
+
+# our project
+import drumseq
+import transcription
+
 
 HELP = '''
 Usage:
@@ -12,17 +22,18 @@ Usage:
 
 Options:
   -h       --help         Show this screen.
-  -i=FILE  --input=FILE   Sets the input file.
+  -i FILE,  --input       Sets the input file.
   -o=FILE  --output=FILE  Sets the output file.
   -t=TEMPO --tempo=TEMPO  [default: 102].
   -s=<arg> --sound=<arg>  [default: 'SN'].
   -p=PORT  --port=PORT    [default: 'TiMiditiy port 0'].
        '''
 
-class DreamSequencer(drumseq_orig.Sequencer):
-    
+class DreamSequencer(drumseq.Sequencer):
+    ''' wrapper around drumseq's Sequencer object which has an API closer to what we want to use '''
+
     def __init__(self, midiout, pattern, bpm=100, channel=9, volume=127):
-        super(Sequencer, self).__init__()
+        super().__init__(midiout, pattern, bpm, channel, volume)
         self.midiout = midiout
         self.bpm = max(20, min(bpm, 400))
         self.interval = 15. / self.bpm
@@ -37,64 +48,56 @@ class DreamSequencer(drumseq_orig.Sequencer):
         self.done = True
         self.join()
 
-class MidiOut(contextlib.AbstractContextManager): # check docs for this
+
+class MidiOut(): # check docs for this
     '''
     Will instatiate the drumseq.Sequencer with the
     parsed pattern from user input (shell).
     and play notes when .play() is called
     '''
 
-    def __init__(self, input_pattern=None, port_desc='TiMidity port 0', **kwargs):
+    def __init__(self, input_pattern=None, port_desc=None, **kwargs):
         '''
-        This will take most of the functionality of __enter__ 
+        This will take most of the functionality of __enter__
         i.e. - it will load an instance of the seqquencer with the pattern.
         Play will actually call sequencer (see below)
         '''
 
         if not input_pattern:
             raise TypeError('must have input_pattern to play!')
-        
-        self.port_name = port_desc #Double check this 
+
         self.drumseq_pattern = self.parse_drummer(input_pattern) # change name to make sense
 
         try:
             midiout, port_name = open_midioutput(
-                args.port,
+                port_desc,
                 api=rtmidi.API_RTMIDI_DUMMY, # play around with these
                 client_name="syncopython",
-                port_name="MIDI Out") 
+            )
 
         except (EOFError, KeyboardInterrupt):
             return
 
-        seq = DreamSequencer(midiout, pattern)
+        self.sequencer = DreamSequencer(midiout, self.drumseq_pattern)
 
-        print("Playing drum loop at %.1f BPM, press Control-C to quit." % seq.bpm)
 
-        try:
-            while True:
-                sleep(1)
-        except KeyboardInterrupt:
-            print('')
-        finally:
-            seq.done = True  # And kill it.
-            seq.join()
-            del midiout
-        print("Done")
 
     def parse_drummer(self, input_pattern):
-        t = Transcription(input_pattern)
+        t = transcription.Transcription(input_pattern)
         return t.output_drumseq()
 
-    def __enter__(self): # __init__ can serve as __enter__ 
-        pass
+    def __enter__(self): # __init__ can serve as __enter__
+        return self
 
     def __exit__(self, exec_type, exec_value, traceback):
         '''
         catches errors and handles them a certain way
-        Clean up transcription, midi ports, etc. 
+        Clean up transcription, midi ports, etc.
         you can re raise the exception here to see what happened
         '''
+        self.stop()
+
+    def stop(self):
         self.sequencer.stop()
 
     def play(self):
@@ -106,7 +109,16 @@ class MidiOut(contextlib.AbstractContextManager): # check docs for this
                 start extends the Thread method
             whatever actually makes the thing run
         '''
-        drumseq.Sequencer(pattern_stream)
+        print("Playing drum loop at %.1f BPM, press Control-C to quit." % self.sequencer.bpm)
+        self.sequencer.play()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print('')
+        finally:
+            self.stop()
+
 
 class MidiFileOut():
     '''
@@ -121,26 +133,21 @@ class MidiFileOut():
         self.pattern_stream = pattern_stream
 
 if __name__ == "__main__":
-    
+
     arguments = docopt.docopt(HELP, version='Syncoi v0.1')
-    from pprint import pprint; print('arguments:'); pprint(arguments)
     # optionally transform our input file to what drumseq.Sequencer expects
 
     # if arguments['midi']:
     #      output = MidiOut(**arguments)
     # print(arguments)
 
-    # pass input to parser and get something suitable for drumseq
-    parser = drummer.Drummer(input_pattern=arguments['--input'])
-    drumseq_pattern = parser.output_drumseq()
-    print(f"drumseq_pattern: '{drumseq_pattern}'")
-
     # decides whatthe output used with 'with' will be
-    output_class = None
+    OutputClass = None
     if arguments['-o'] or arguments['--output']:
-        output_class = MidiFileOut
+        OutputClass = MidiFileOut
     else:
-        output_class = MidiOut
+        OutputClass = MidiOut
+    print("arguments:", arguments)
 
-    with output_class(drumseq_pattern=drumseq_pattern) as out:
+    with OutputClass(input_pattern=arguments['-i']) as out:
         out.play()
