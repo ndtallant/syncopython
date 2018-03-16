9c7f819efee55eb4f3cc0a46d07dd72025d33d34
Nick Tallant <ndtallant@uchicago.edu>
2018-03-16 14:21:30 -0500

    Redid testing after rm the file -_-

--- test_transcription.py
+++ test_transcription.py
@@ -1,6 +1,6 @@
 '''
 Classes that form the basis for syncopython
-The rhythm string for each limb will be parsed and joined
+The rhythm str for each limb will be parsed and joined
 to output to drumseq.py as I develop a backend.
 '''
 import re
@@ -15,34 +15,28 @@ class TestTranscription(unittest.TestCase):
     def setUp(self):
         self.t = Transcription()
 
-    def complex_transcription(self):
-        input_str = '''
-        36 | 1 & ,    ,3 & ,     | Bassdrum
-        38 |     ,2   ,    ,4    | Snare
-        42 | 1e&a,2e&a,3e&a,4e&a | Closed HH
-        '''
+    def test_complex_transcription(self):
+        input_str = ('36 | 1 & ,    ,3 & ,     | Bassdrum\n'
+                     '38 |     ,2  a,    ,4e a | Snare\n'
+                     '42 | 1e&a,2e&a,3e&a,4e&a | Closed HH')
 
-        self.t.feed(input_str)
-        out_string = t.output_drumseq()
+        t = Transcription(input_str)        
+        out_str = t.output_drumseq()
 
-        expected_out = '''
-        #  1...2...3...4...\n36 x.x.......x..x.. Bassdrum\n38 ....x..x.x.xx..x Snare\ \n
-        42 xxxxx.x.xxxxx.xx Closed HH
-        '''
-        assert out_string.strip() == expected_out.strip()
-        # Black box testing - only public use
+        expected_out = ('#  1...2...3...4...\n'
+                        '36 x.x.....x.x.....  Bassdrum\n'
+                        '38 ....x..x....xx.x  Snare\n'
+                        '42 xxxxxxxxxxxxxxxx  Closed HH')
+
+        assert out_str.strip() == expected_out.strip()
  
     def test_simple_transcription(self):
-        input_string = '''
-        36 | 1&, ,e,e | Bassdrum
-        '''.strip()
+        input_str = '36 | 1 & , , e , e | Bassdrum'
 
-        t = Transcription(input_string)
-        out_string = t.output_drumseq()
-        expected_out = '''
-        #  1...2...3...4...\n36 x.x......x...x..  Bassdrum
-        '''
-        print('input_string: ' + input_string)
-        print('out_string: ' + out_string)
-        self.assertEqual(expected_out.strip(), out_string.strip())
-        # Black box testing - only public use
+        t = Transcription(input_str)
+        out_str = t.output_drumseq()
+        
+        expected_out = ('#  1...2...3...4...\n'
+                        '36 x.x......x...x..  Bassdrum')
+        
+        self.assertEqual(expected_out.strip(), out_str.strip())
