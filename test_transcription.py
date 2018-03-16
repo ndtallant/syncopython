abaf3c811e0f497bc54123693b4ce916648d1ecf
Nick Tallant <ndtallant@uchicago.edu>
2018-03-16 14:29:21 -0500

    added test

--- test_transcription.py
+++ test_transcription.py
@@ -33,10 +33,27 @@ class TestTranscription(unittest.TestCase):
     def test_simple_transcription(self):
         input_str = '36 | 1 & , , e , e | Bassdrum'
 
-        t = Transcription(input_str)
-        out_str = t.output_drumseq()
-        
+                
         expected_out = ('#  1...2...3...4...\n'
                         '36 x.x......x...x..  Bassdrum')
         
+        t = Transcription(input_str)
+        out_str = t.output_drumseq()
+
         self.assertEqual(expected_out.strip(), out_str.strip())
+
+    def test_linear(self):
+        input_str = ('36 | 1  a,    ,3  a,     | Bassdrum\n'
+                     '38 |  e  ,2 & , e   ,4 &  | Snare\n'
+                     '42 |   & , e a,  & , e a | Closed HH')
+        
+        expected_out = ('#  1...2...3...4...\n'
+                        '36 x..x....x..x....  Bassdrum\n'
+                        '38 .x..x.x..x..x.x.  Snare\n'
+                        '42 ..x..x.x..x..x.x  Closed HH')
+
+        t = Transcription(input_str)
+        out_str = t.output_drumseq()
+
+        self.assertEqual(expected_out.strip(), out_str.strip())
+
