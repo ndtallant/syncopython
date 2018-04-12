e008da2b374f6f9315685e55082ccc898cf6aeab
Nick Tallant <ndtallant@uchicago.edu>
2018-04-12 10:23:50 -0500

    Cleaned up instrument prompting - shifting to a general prompt

--- sandbox/prompt_testing.py
+++ sandbox/prompt_testing.py
@@ -10,26 +10,43 @@ from prompt_toolkit.history import InMemoryHistory
 from prompt_toolkit.contrib.completers import WordCompleter
 
 inst_list = ['Hi-Hat', 'Snare', 'Kick']
-instrumentation = WordCompleter(inst_list, ignore_case=True)
- 
-def main():
+inst_completer= WordCompleter(inst_list, ignore_case=True)
+
+command_completer = WordCompleter(['Add Drumkit',
+                                   'Add Instrument',
+                                   'Change Instrument Rhythm',
+                                   'Delete Instrument'],
+                                   ignore_case=True)
+
+def prompt_inst():
+    '''
+    This function prompts the user for an instrument,
+    and then prompts for a new rhythm.
+    '''
+    inst_entry = prompt("> ", completer=inst_completer) #pass history in here or nah?
+    
+    if inst_entry in inst_list:
+        rs = prompt("Rhythm > ") #pass history in here or nah?
+        print(inst_entry,':',rs)
+    else:
+        print('Enter Hi-Hat, Snare, or Kick')
+
+def change_inst():
+    pass
+
+def rm_inst():
+    pass
+
+def go():
     history = InMemoryHistory()
 
     while True:
         try:
-            entry = prompt("> ", history=history, completer=instrumentation)
-            
-            if entry in inst_list:
-                rs = prompt("Rhythm > ", history=history)
-                print(entry,':',rs)
-
-            else:
-                print('Enter Hi-Hat, Snare, or Kick')
-
+            prompt_inst()    
         except EOFError:
             break  # Control-D pressed.
 
     print('Sweet Beats!')
 
 if __name__ == '__main__':
-    main()
+    go()
